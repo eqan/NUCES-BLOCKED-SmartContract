b34a36e7cbc92e3b7f5c31f5e8c1742e1b6c3200
eqan <eqan.ahmad123@gmail.com>
2023-03-25 00:40:38 +0500

    Adding sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,10 @@ chai.use(eth.solidity);
 module.exports = {
   solidity: '0.8.0',
   networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/rMlBxpd3_3GTFrypMWfp4kjPNQgmr6Pk',
+      accounts: ['9406eb97ace8d76d7958a310051ecf9989f56f568c05012fa0b376add952e84b'],
+    },
     goreli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/ezFT7gurVy9OCsrKYwKnbb0PI6JgSUiI',
       accounts: ['9406eb97ace8d76d7958a310051ecf9989f56f568c05012fa0b376add952e84b'],
